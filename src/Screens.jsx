3f100840caf086d2263eacde413e10a366e6bb05
davik20 <65835906+Meka-tech@users.noreply.github.com>
2024-02-23 22:56:04 +0100

    leaderboard functionality

--- src/Screens.jsx
+++ src/Screens.jsx
@@ -17,9 +17,17 @@ export const possibleTileContents = [
 ];
 
 export function StartScreen({ start }) {
+  const [leaderboard, setLeaderboard] = useState([]);
+  const [showLeaderboard, setShowLeaderboard] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("leaderboard")) {
+      setLeaderboard(JSON.parse(localStorage.getItem("leaderboard")));
+    }
+  }, []);
   return (
     <>
-      <main className="h-screen w-full flex flex-col items-center justify-center">
+      <main className="h-screen w-full flex flex-col items-center justify-center relative overflow-hidden">
         <div className="w-4/5 rounded-lg mx-auto flex flex-col justify-center items-center p-3 bg-pink-100/50 text-pink-500 h-3/5">
           <h1 className="text-4xl font-bold mb-10">Memory</h1>
           <h2 className="text-lg font-semibold mb-10">
@@ -31,6 +39,54 @@ export function StartScreen({ start }) {
               className="bg-gradient-to-b from-pink-400 to-pink-500 rounded-full h-10 w-32 text-2xl font-semibold text-center text-white shadow-xl active:from-pink-500 active:to-pink-500"
             >
               Play
+            </button>
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                setShowLeaderboard(true);
+              }}
+              className="bg-gradient-to-b from-pink-400 to-pink-500 rounded-full h-10 w-fit px-8 text-2xl font-semibold text-center text-white shadow-xl active:from-pink-500 active:to-pink-500 mt-4"
+            >
+              Leaderboard
+            </button>
+          </div>
+        </div>
+
+        <div
+          className={`w-full h-screen absolute z-10 bg-white flex items-center p-2 duration-300 ${
+            !showLeaderboard && "translate-x-full"
+          }`}
+        >
+          <div className="w-4/5 rounded-lg mx-auto flex flex-col  items-center p-3 bg-pink-500/10 h-4/5 ">
+            <h1 className="text-3xl font-bold text-pink-500 mb-4">
+              Leaderboard
+            </h1>
+            <div className="w-full flex flex-col items-center h-4/5 px-2 overflow-y-scroll">
+              <div className="w-full flex justify-between items-center mb-2 text-pink-500 font-bold">
+                <h2>Name</h2>
+                <h2>Time</h2>
+              </div>
+
+              {leaderboard.map((item, i) => {
+                return (
+                  <div
+                    className="w-full rounded-lg px-4 py-1 bg-pink-400 text-white flex items-center justify-between"
+                    key={i}
+                  >
+                    <h2 className="font-bold text-lg ">{item.name}</h2>
+                    <h2 className="font-bold">{item.time} s</h2>
+                  </div>
+                );
+              })}
+            </div>
+            <button
+              className="bg-gradient-to-b from-pink-400 to-pink-500 rounded-xl h-10 w-fit px-4 text-lg font-semibold text-center text-white shadow-xl active:from-pink-500 active:to-pink-500 mt-4 mr-auto"
+              onClick={() => {
+                setShowLeaderboard(false);
+              }}
+            >
+              Back
             </button>
           </div>
         </div>
@@ -48,10 +104,24 @@ export function PlayScreen({ end }) {
   const [HintTile, setHintTile] = useState("");
   const [hasHint, setHasHint] = useState(true);
   const [hasWon, setHasWon] = useState(false);
+  const [betterTime, setBetterTime] = useState(false);
+  const [leaderboard, setLeaderboard] = useState([]);
+  const [username, setUsername] = useState("");
+  const [bestTime, setBestTime] = useState(0);
   const tileCount = 16;
 
-  //
+  useEffect(() => {
+    if (localStorage.getItem("leaderboard")) {
+      setLeaderboard(JSON.parse(localStorage.getItem("leaderboard")));
+      setBestTime(
+        leaderboard.find(
+          (entry) => entry.time === Math.max(...leaderboard.map((e) => e.time))
+        )
+      );
+    }
+  }, []);
 
+  //timer countDown
   useEffect(() => {
     if (isPlaying) {
       const TimerInterval = setInterval(() => {
@@ -62,12 +132,14 @@ export function PlayScreen({ end }) {
     }
   }, [timer]);
 
+  // player loses
   useEffect(() => {
     if (failCount === 5 || timer === 1) {
       setIsPlaying(false);
     }
   }, [failCount, timer]);
 
+  //restart or play again
   const Restart = () => {
     setFailCount(0);
     setTryCount(0);
@@ -76,13 +148,28 @@ export function PlayScreen({ end }) {
     setHasHint(true);
     setHintTile("");
     setHasWon(false);
+    setBetterTime(false);
     setIsPlaying(true);
   };
 
+  //end game
   const Endgame = () => {
     end();
   };
 
+  //save new best time
+  const SaveTime = () => {
+    if (username === "") {
+      return;
+    }
+    localStorage.setItem(
+      "leaderboard",
+      JSON.stringify([{ name: username, time: timer }, ...leaderboard])
+    );
+    Endgame();
+  };
+
+  //shuffle tiles randomly
   const Shuffle = () => {
     const arrayCopy = [...tiles];
 
@@ -159,7 +246,11 @@ export function PlayScreen({ end }) {
 
           // If all tiles are matched, the game is over.
           if (newTiles.every((tile) => tile.state === "matched")) {
+            setIsPlaying(false);
             setHasWon(true);
+            if (timer > bestTime) {
+              setTimeout(() => setBetterTime(true), [1000]);
+            }
           }
 
           return newTiles;
@@ -175,18 +266,39 @@ export function PlayScreen({ end }) {
     });
   };
 
+  //get one hint per game
   const GetHint = () => {
+    let length = tiles.length;
+    const randomTile = Math.random(0, length + 1);
     if (hasHint) {
-      let length = tiles.length;
-      setHintTile(tiles[Math.floor(Math.random(0, length + 1))]);
+      setHintTile(tiles[Math.floor(randomTile)]);
       setHasHint(false);
     }
   };
 
   getTiles();
+
   return (
     <>
       <main className="h-screen w-full flex flex-col items-center justify-center relative overflow-hidden">
+        <div
+          className={`w-full flex items-center justify-center mb-10 absolute top-12 duration-150 ${
+            isPlaying ? " -translate-x-full" : ""
+          } `}
+        >
+          <div
+            className={`px-2 py-1 rounded-lg ${
+              hasWon
+                ? "bg-sky-500/10 text-sky-500"
+                : "bg-red-500/10 text-red-500"
+            } `}
+          >
+            <h1 className="font-bold text-xl ">
+              {hasWon ? "You Won !" : "You Lost !"}
+            </h1>
+          </div>
+        </div>
+
         <div className="flex w-full items-center mb-10 px-4">
           <h2 className="text-lg font-semibold  text-blue-700">
             Tries
@@ -249,6 +361,32 @@ export function PlayScreen({ end }) {
           >
             Shuffle
           </button>
+        </div>
+        <div
+          className={`absolute w-full h-screen top-0 z-10 bg-white flex flex-col items-center justify-center duration-150 
+${!betterTime && "translate-y-full"}`}
+        >
+          <div className="w-4/5 flex flex-col items-center p-2 rounded-xl bg-sky-500/5 ">
+            <h1 className="text-2xl font-bold mb-10 text-sky-500">
+              New Best-Time
+            </h1>
+            <div className="flex items-center justify-between w-60">
+              <input
+                placeholder="Username"
+                className=" bg-blue-500/10 rounded-lg p-2  placeholder:text-blue-500/50 text-lg font-bold text-blue-500 focus:outline-none w-4/5"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+
+              <h2 className="text-xl font-bold text-sky-500">{timer}s</h2>
+            </div>
+            <button
+              className="h-10 w-32 px-4 rounded-lg bg-sky-500 text-white mt-10 \n font-bold"
+              onClick={SaveTime}
+            >
+              Save
+            </button>
+          </div>
         </div>
       </main>
     </>
